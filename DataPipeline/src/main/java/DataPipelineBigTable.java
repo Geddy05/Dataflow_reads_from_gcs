3b8fb0efdbef63c62c030949573a0c66bb3e9c3b
Geddy05 <108874650+Geddy05@users.noreply.github.com>
2022-11-09 15:03:38 +0100

    Added Comments to the BigTable Example

--- DataPipeline/src/main/java/DataPipelineBigTable.java
+++ DataPipeline/src/main/java/DataPipelineBigTable.java
@@ -1,22 +1,15 @@
 
-import com.google.api.services.bigquery.model.TableRow;
 import com.google.cloud.bigtable.beam.CloudBigtableIO;
 import com.google.cloud.bigtable.beam.CloudBigtableTableConfiguration;
 import models.Player;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.options.Default;
-import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.windowing.FixedWindows;
-import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.joda.time.Duration;
 import pTransforms.JsonToPlayerPipeline;
 import pTransforms.PollingGCSPipeline;
 
@@ -26,8 +19,14 @@ public class DataPipelineBigTable {
     @ProcessElement
     public void processElement(@Element Player player, OutputReceiver<Mutation> out) {
       long timestamp = System.currentTimeMillis();
+      // Create the row key for Bigtable. For this example we are using the key format teamId#UserId#Timestamp
+      // This is not according the best practices for the row-key design more about the row-key design can be found here:
+      // https://cloud.google.com/bigtable/docs/schema-design#row-keys
       Put row = new Put(Bytes.toBytes(player.teamId+"#"+player.userId+"#"+player.timestamp.toString()));
 
+      // Add the column to the Row we created earlier.
+      // The add column function needs te following parameter family, qualifier, timestamp, value
+      // more about schema design: https://cloud.google.com/bigtable/docs/schema-design
       row.addColumn(
           Bytes.toBytes("stats_summary"),
           Bytes.toBytes("username"),
@@ -62,14 +61,17 @@ public class DataPipelineBigTable {
         PipelineOptionsFactory.fromArgs(args).withValidation().as(BigtableOptions.class);
     options.setStreaming(true);
 
-
+    // Create BigTable configurations.
+    // We need to set the Project ID, Instance ID and TableId.
     CloudBigtableTableConfiguration bigtableTableConfig =
         new CloudBigtableTableConfiguration.Builder()
             .withProjectId(options.getBigtableProjectId())
             .withInstanceId(options.getBigtableInstanceId())
             .withTableId(options.getBigtableTableId())
             .build();
-    Pipeline p = Pipeline.create();
+
+    // Create the pipeline with the given options.
+    Pipeline p = Pipeline.create(options);
 
     PCollection<Player> playerScore = p
             // rfcStartDateTime: Only read files with an updated timestamp greater than the rfcStartDateTime.
@@ -79,8 +81,9 @@ public class DataPipelineBigTable {
             .apply("File to Players", new JsonToPlayerPipeline());
 
     playerScore
-        .apply("window", Window.into(FixedWindows.of(Duration.standardSeconds(10))))
+        // Create a put Operation based on the input
         .apply("Player to Put Operation", ParDo.of(new PlayerToBigtablePut()))
+        // Write the given put operations. It used the configurations given earlier.
         .apply(CloudBigtableIO.writeToTable(bigtableTableConfig));
 
     p.run().waitUntilFinish();
